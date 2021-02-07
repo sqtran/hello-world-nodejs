4ff2bfdd4fc502354f22118338fca327bf0f3d34
Steve Tran <stran@redhat.com>
2021-02-06 19:55:08 -0500

    Switch to Restify module

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
-var http = require('http');
+var restify = require('restify');
 
-var server = http.createServer(function(request, response) {
-    
-    const { headers } = request;
+function respond(req, res, next) {
+    res.charSet('utf-8');
+    res.contentType = 'json';
+    res.send(200, {hello: 'world'});
+    next();
+}
 
-    var message = `hello world!\n`;
+function respond_headers(req, res, next){
+    const { headers } = req;
+
+    var message = "";
     for (const h in headers) {
-        message += `headers[${h}] = ${headers[h]}\n`;
+        console.log(`[${h}] = ${headers[h]}`);
+        message += `[${h}]=${headers[h]} `;
     }
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end(message);
-    console.log(message);
+    res.charSet('utf-8');
+    res.send(200, message);
+    next();
+}
+
+var server = restify.createServer();
+
+server.get('/', respond);
+server.get('/headers', respond_headers);
+
+server.listen(8080, function() {
+  console.log('%s listening at %s', server.name, server.url);
 });
-
-var port = 8088;
-server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
